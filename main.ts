ddd84f18465a63805522c0bdce0030f3004cec55
timarcelo <78170752+timarcelo@users.noreply.github.com>
2024-07-29 13:57:13 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     if (Seguidor_de_Linha.readUma(Umsensor.branco, 605)) {
-        robotbit.MotorRun(Seguidor_de_Linha.Motors.M1A, 123)
+        Seguidor_de_Linha.MotorRun(Seguidor_de_Linha.Motors.M1A, 123)
     } else {
-        robotbit.MotorRun(Seguidor_de_Linha.Motors.M2A, 123)
+        Seguidor_de_Linha.MotorRun(Seguidor_de_Linha.Motors.M2A, 123)
     }
 })
